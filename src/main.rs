3bc0487504fc10232b06abf03b9137acd02f7534
alvaro0106 <alvarollvv@gmail.com>
2023-01-24 00:38:13 -0600

    Commit: Paso 4 Argumentos linea de comandos

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::env::{args, Args};
+
 fn main() {
-    println!("Hello, world!");
+    let mut args: Args = args();
+
+    // el primer argumento es la ubicacion del binario compilado, asi que saltar
+    let primero: String = args.nth(1).unwrap();
+
+    // Despues de acceder al segundo argumento, el elemento iterador siguiente se convierte en el primero
+    let operador: String = args.nth(0).unwrap();
+    let segundo: String = args.nth(0).unwrap();
+
+    println!("{} {} {} ", primero, operador, segundo);
+
+
 }
