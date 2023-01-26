c4c7a9b7f2fdacb0fa75db61c7d4a4e3b8ddeb7c
alvaro0106 <alvarollvv@gmail.com>
2023-01-25 18:16:58 -0600

    Progra finsh

--- README.md
+++ README.md
@@ -2,3 +2,42 @@
 
 Vincular Github con Subl
 https://docs.github.com/es/get-started/getting-started-with-git/associating-text-editors-with-git
+
+
+### Execute program
+
+```
+cargo run -- 1 + 1
+```
+
+### step 5. analyzer string in number
+
+Method parse convert a str a int
+
+### step 6. To perform arithmetic operation
+
+rust to use the operations standard sum, mult, and div.
+for operations manager define a functions operate that take 3: operation whit char  and 2 numbers whit f32
+
+now to call operate with operator, first convert in char this to do method chars, in the struct String.
+
+### formating the exit
+to get the result to want, the variables primer_numero, segundo_numero y resultado should formating, using format! macro to create a String.
+
+
+### gather all
+
+for create code of a executable binary:
+
+```
+cargo build --release
+```
+the flag -- release indicate to cargo that compil the binary in mode -- release
+
+the binary this constructed in director target/release. for execute binary and test the application: 
+
+```
+target/release/calculator 1 + 1
+```
+
+ 
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,32 @@
 use std::env::{args, Args};
 
 fn main() {
-    let mut args: Args = args();
+  let mut args: Args = args();
 
-    // el primer argumento es la ubicacion del binario compilado, asi que saltar
-    let primero: String = args.nth(1).unwrap();
+  let primero: String = args.nth(1).unwrap();
+  let operador: char = args.nth(0).unwrap().chars().next().unwrap();
+  let segundo: String = args.nth(0).unwrap();
 
-    // Despues de acceder al segundo argumento, el elemento iterador siguiente se convierte en el primero
-    let operador: String = args.nth(0).unwrap();
-    let segundo: String = args.nth(0).unwrap();
+  let primer_numero = primero.parse::<f32>().unwrap();
+  let segundo_numero = segundo.parse::<f32>().unwrap();
+  let resultado = operate(operador, primer_numero, segundo_numero);
 
-    println!("{} {} {} ", primero, operador, segundo);
+  println!("{:?}", output(primer_numero, operador, segundo_numero, resultado) );
+}
+ 
+fn output(primer_numero: f32, operador: char, segundo_numero: f32, resultado: f32) -> String {
+  format!(
+    "{} {} {} = {}",
+    primer_numero, operador, segundo_numero, resultado
+  )
+}
 
-
+fn operate(operador: char, primer_numero: f32, segundo_numero: f32) -> f32 {
+  match operador {
+    '+' => primer_numero + segundo_numero,
+    '-' => primer_numero - segundo_numero,
+    '/' => primer_numero / segundo_numero,
+    '*' | 'X' | 'x' => primer_numero * segundo_numero,
+    _ => panic!("Operador utilizado no válido."),
+  }
 }
